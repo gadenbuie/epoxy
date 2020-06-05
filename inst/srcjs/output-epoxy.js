9814dda4e0a4741ff62cfbdd33d9f26f51cd2039
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-06-04 21:56:03 -0400

    epoxyHTML output handles arrays

--- inst/srcjs/output-epoxy.js
+++ inst/srcjs/output-epoxy.js
@@ -4,10 +4,32 @@ $.extend(epoxyOutputBinding, {
     return $(scope).find('.epoxy-html');
   },
   renderValue: function(el, data) {
+    // remove copies of epoxyItem
+    let elCopies = el.querySelectorAll('[data-epoxy-copy]')
+    elCopies.forEach(e => e.parentElement.removeChild(e))
+
     let items = el.querySelectorAll('[data-epoxy-item]');
-    [...items].forEach(item => {
+    items.forEach(item => {
       item.classList.remove('epoxy-item__placeholder');
-      item.innerHTML = data[item.dataset.epoxyItem];
+      let itemName = item.dataset.epoxyItem;
+
+      let itemData = data[itemName];
+      if (itemData instanceof Array) {
+        let lastItem = item;
+        item.innerHTML = itemData[0];
+        itemParent = item.parentElement;
+        itemData = itemData.slice(1);
+        for (let itemDataThis of itemData) {
+          const itemNew = item.cloneNode();
+          itemNew.removeAttribute('data-epoxy-item');
+          itemNew.dataset.epoxyCopy = itemName;
+          itemNew.innerHTML = itemDataThis;
+          itemParent.insertBefore(itemNew, lastItem.nextSibling);
+          lastItem = itemNew;
+        }
+      } else {
+        item.innerHTML = itemData;
+      }
     });
     el.classList.remove('epoxy-init');
   },
