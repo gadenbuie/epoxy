001329aa997ad5d0f8e36696ae7d952eea0300c1
Garrick Aden-Buie <garrick@adenbuie.com>
2022-04-23 08:46:26 -0400

    Add support for chained `epoxy_style_*` transformers

---
+++ tests/testthat/test-transformers.R
@@ -0,0 +1,51 @@
+test_that("epoxy_style() works", {
+  expect_equal(
+    glue(
+      "{letters[1:3]&}",
+      .transformer = epoxy_style(
+        "code",
+        epoxy_style_bold,
+        epoxy_style_collapse(last_and = ", ... and ... ")
+      )
+    ),
+    "**`a`**, **`b`**, ... and ... **`c`**"
+  )
+
+  expect_equal(
+    glue(
+      "{letters[1:3]&}",
+      .transformer = epoxy_style(
+        "code",
+        epoxy_style_bold,
+        epoxy_style_collapse(last_and = ", ... and ... "),
+        epoxy_style_wrap("<< ", " >>")
+      )
+    ),
+    "<< **`a`**, **`b`**, ... and ... **`c`** >>"
+  )
+
+  expect_equal(
+    glue(
+      "{letters[1:3]&}",
+      .transformer = epoxy_style(
+        "code",
+        epoxy_style_collapse(last_and = ", ... and ... "),
+        epoxy_style_bold
+      )
+    ),
+    "**`a`, `b`, ... and ... `c`**"
+  )
+
+  last_and <- ", ... and ... "
+  expect_equal(
+    glue(
+      "{letters[1:3]&}",
+      .transformer = epoxy_style(
+        "code",
+        epoxy_style_collapse(last_and = last_and),
+        epoxy_style_bold
+      )
+    ),
+    "**`a`, `b`, ... and ... `c`**"
+  )
+})
