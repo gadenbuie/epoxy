08facc9c418a4c64e9bdf592ae0c5ba082740d6b
Garrick Aden-Buie <garrick@adenbuie.com>
2022-04-23 14:39:55 -0400

    Add one more test

--- tests/testthat/test-transformers.R
+++ tests/testthat/test-transformers.R
@@ -1,6 +1,18 @@
 test_that("epoxy_style() works", {
   expect_equal(
     glue(
+      "{letters[1:3]}",
+      .transformer = epoxy_style(
+        "code",
+        epoxy_style_italic,
+        epoxy_style_bold()
+      )
+    ),
+    sprintf("**_`%s`_**", letters[1:3])
+  )
+
+  expect_equal(
+    glue(
       "{letters[1:3]&}",
       .transformer = epoxy_style(
         "code",
