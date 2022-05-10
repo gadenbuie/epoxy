46b199e6988aabb03d7e45e8772bfb6df061b4e0
Garrick Aden-Buie <garrick@adenbuie.com>
2022-05-09 21:47:00 -0400

    Allow user-supplied formatters, add text formatters, update example

--- R/epoxy_style_format.R
+++ R/epoxy_style_format.R
@@ -6,36 +6,17 @@
 #' expressions in `fmt(expr, label)`, where `label` is the name of a labeller
 #' function from \pkg{scales} (with or without the leading `label_`), e.g.
 #' `"dollar"` or `"label_dollar"` for [scales::label_dollar()]. Short `label`
-#' forms exist for some labellers, e.g. `"$"` for `"dollar"`.
+#' forms exist for some labellers, e.g. `"$"` for `"dollar"`. See the full
+#' listing in the table below.
 #'
 #' ```{r child="man/fragments/airbnb-example.Rmd"}
 #' ```
 #'
-#' ### `fmt()` labellers
+#' ### `fmt()` labels for formatters
 #'
 #' ```{r echo=FALSE}
-#' args <- rlang::fn_fmls_names(epoxy_style_format)
-#' args <- setdiff(args, "transformer")
-#' extras <- labeller_extras()
-#' labellers <- data.frame(
-#'   applies = sprintf(
-#'     "[scales::label_%s()]",
-#'     c(unname(extras), args)
-#'   ),
-#'   label = c(names(extras), args)
-#' )
-#' labellers <- labellers[order(labellers$applies, labellers$label), ]
-#' labellers <- aggregate(
-#'   labellers,
-#'   list(applies = labellers$applies),
-#'   function(label) {
-#'     if (grepl("^\\[", label[1])) {
-#'       return(label[1])
-#'     }
-#'     paste0('`"', label, '"`', collapse = ", ")
-#'   }
-#' )
-#' knitr::kable(labellers[, 3:2], row.names = FALSE, col.names = c("`label`", "Applies formatting with"))
+#' labellers <- labellers_summarize()
+#' knitr::kable(labellers[, 2:1], row.names = FALSE, col.names = c("`label`", "Applies formatting with"))
 #' ```
 #'
 #' @export
@@ -56,11 +37,15 @@ epoxy_style_format  <- function(
   pvalue      = scales::label_pvalue(),
   scientific  = scales::label_scientific(),
   wrap        = scales::label_wrap(width = 80),
+  uppercase   = toupper,
+  lowercase   = tolower,
+  titlecase   = tools::toTitleCase,
+  ...,
   transformer = glue::identity_transformer
 ) {
   rlang::check_installed("scales")
 
-  labellers <- labellers_list()
+  labellers <- labellers_list(...)
   fmt <- labeller_factory(labellers)
 
   function(text, envir) {
@@ -71,14 +56,14 @@ epoxy_style_format  <- function(
 
 labellers_names <- function() {
   labeller_arg_names <- rlang::fn_fmls_names(epoxy_style_format)
-  setdiff(labeller_arg_names, "transformer")
+  setdiff(labeller_arg_names, c("...", "transformer"))
 }
 
 labellers_list <- function(env = rlang::caller_env(), ...) {
-  labeller_names <- labellers_names()
+  dots <- list(...)
+  labeller_names <- c(labellers_names(), names(dots))
   defaults <- rlang::fn_fmls(epoxy_style_format)[labellers_names()]
   defaults <- purrr::map(defaults, eval)
-  dots <- list(...)
 
   purrr::map(
     purrr::set_names(labeller_names),
@@ -108,35 +93,82 @@ labeller_extras <- function() {
     "pct"  = "percent",
     "%"    = "percent",
     "p"    = "pvalue",
-    "si"   = "scientific"
+    "si"   = "scientific",
+    "uc"   = "uppercase",
+    "lc"   = "lowercase",
+    "tc"   = "titlecase"
   )
 }
 
 labeller_factory <- function(labellers = labellers_list()) {
   force(labellers)
 
+  # labeller_extras <- sub("^label_", "", labeller_extras())
   labeller_extras <- labeller_extras()
+  default_names <- labellers_names()
+
+  match_label <- function(label) {
+    call <- rlang::caller_call()
+    label <- sub("^label_", "", label)
+
+    if (label %in% names(labeller_extras)) {
+      labeller_extras[label]
+    } else if (rlang::has_name(labellers, label)) {
+      label
+    } else {
+      tryCatch(
+        match.arg(label, labellers_names()),
+        error = function(err) {
+          rlang::abort(
+            glue("Unknown format `label` '{label}'"),
+            call = call
+          )
+        }
+      )
+    }
+  }
 
   function(text, label, ...) {
     if (length(label) != 1) {
       rlang::abort("`label` must be a single character string")
     }
 
-    label <- sub("^label_", "", label)
-
-    if (label %in% names(labeller_extras)) {
-      label <- labeller_extras[label]
-    } else {
-      label <- tryCatch(
-        match.arg(label, labellers_names()),
-        error = function(err) {
-          rlang::abort(
-            glue("Unknown `scale` '{scale}'")
-          )
-        }
-      )
-    }
+    label <- match_label(label)
 
     labellers[[label]](text, ...)
   }
+}
+
+labellers_summarize <- function() {
+  args <- rlang::fn_fmls(epoxy_style_format)[labellers_names()]
+  args <- purrr::modify_if(args, rlang::is_symbol, rlang::expr_text)
+  args <- purrr::modify_if(args, rlang::is_call_simple, rlang::call_name)
+  args <- purrr::modify_if(args, purrr::negate(rlang::is_string), rlang::expr_text)
+
+  extras <- labeller_extras()
+  extras_fns <- args[unname(extras)]
+  labels <- c(names(extras), names(args))
+
+  fns <- unlist(c(unname(extras_fns), unname(args)))
+  fns[grepl("^label_", fns)] <- paste0("scales::", fns[grepl("^label_", fns)])
+  fns <- sprintf("[%s()]", fns)
+
+  labellers <- data.frame(
+    applies = fns,
+    label = labels
+  )
+
+  labellers <- aggregate(
+    labellers[-1],
+    list(applies = labellers$applies),
+    function(label) {
+      if (grepl("^\\[", label[1])) {
+        return(label[1])
+      }
+      paste0('`"', label, '"`', collapse = ", ")
+    }
+  )
+
+  rownames(labellers) <- labellers$applies
+  labellers[fns[-seq_along(extras)], ]
 }
