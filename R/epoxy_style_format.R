03826b94eb0dd2311e3689fd3dd5a320bef88ac0
Garrick Aden-Buie <garrick@adenbuie.com>
2022-05-14 09:24:34 -0400

    tweak examples

--- R/epoxy_style_format.R
+++ R/epoxy_style_format.R
@@ -20,25 +20,28 @@
 #' ```
 #'
 #' @examples
+#' revenue <- 0.2123
+#' sales <- 42000.134
 #' glue::glue(
-#'   '{fmt(0.2, "%")} of revenue generates {fmt(42000, "$")} in profits.',
+#'   '{fmt(revenue, "%")} of revenue generates {fmt(sales, "$")} in profits.',
 #'   .transformer = epoxy_style_format()
-#' )
-#'
-#' # Add your own formatting functions
-#' glue::glue(
-#'   '<https://example.com?q={fmt("search terms go here", "url")}>',
-#'   .transformer = epoxy_style_format(
-#'     url = utils::URLencode
-#'   )
 #' )
 #'
 #' # To set labeller options, provide the label calls
 #' glue::glue(
-#'   '{fmt(0.2123, "%")} of revenue generates {fmt(42000.134, "$")} in profits.',
+#'   '{fmt(revenue, "%")} of revenue generates {fmt(sales, "$")} in profits.',
 #'   .transformer = epoxy_style_format(
 #'     percent = scales::label_percent(accuracy = 0.1),
 #'     dollar = scales::label_dollar(accuracy = 10)
+#'   )
+#' )
+#'
+#' # Add your own formatting functions
+#' search <- "why are cats scared of cucumbers"
+#' glue::glue(
+#'   '<https://example.com?q={fmt(search, "url")}>',
+#'   .transformer = epoxy_style_format(
+#'     url = utils::URLencode
 #'   )
 #' )
 #'
