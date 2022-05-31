fba7e44f402a89d937dfe1d1350b4572a4a53843
Garrick Aden-Buie <garrick@adenbuie.com>
2022-05-31 09:53:13 -0400

    fix: Don't alias `epoxy-package` to `epoxy`

--- R/epoxy-package.R
+++ R/epoxy-package.R
@@ -1,5 +1,6 @@
 #' @importFrom glue glue glue_collapse glue_data
 #' @keywords internal
+#' @aliases epoxy-package NULL
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
