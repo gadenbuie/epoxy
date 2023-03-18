d0c33177aae8572fc7371e5d05d28826e5cce8e6
gadenbuie <gadenbuie@users.noreply.github.com>
2023-03-18 13:38:41 +0000

    Fix indents and invisible characters (GitHub Actions)

--- R/transformers.R
+++ R/transformers.R
@@ -20,46 +20,6 @@
 #'   the `syntax` to one of `"markdown"`, `"html"` or `"latex"`.
 #'
 #' @examples
-#' glue::glue("{letters[1:3]&}", .transformer = epoxy_style("bold", "collapse"))
-#' glue::glue("{letters[1:3]&}", .transformer = epoxy_style("collapse", "bold"))
-#'
-#' # In an epoxy_html chunk...
-#' # Note that you don't have to set `syntax = "html"`, it just knows
-#' glue::glue(
-#'   "{letters[1:3]&}",
-#'   .transformer = epoxy_style("bold", "collapse", syntax = "html")
-#' )
-#'
-#' # Or in an epoxy_latex chunk...
-#' glue::glue(
-#'   "{letters[1:3]&}",
-#'   .transformer = epoxy_style("bold", "collapse", syntax = "latex")
-#' )
-#'
-#' # Other Transfomers ----
-#'
-#' # Apply `format()` to all replacements
-# number <- 1.234234234
-# glue::glue(
-#   "{number}",
-#   .transformer = epoxy_style_format(digits = 4)
-# )
-#
-# # Apply _any_ function to all replacements
-# glue::glue(
-#   "{number}",
-#   .transformer = epoxy_style_apply(round, digits = 0)
-# )
-#
-# glue::glue(
-#   "{number}",
-#   .transformer = epoxy_style(
-#     epoxy_style_apply(~ .x * 100),
-#     epoxy_style_apply(round, digits = 2),
-#     epoxy_style_apply(~ paste0(.x, "%"))
-#   )
-# )
-#'
 #' @param ... A list of style functions, e.g. `epoxy_style_bold` or the name of
 #'   a style function, e.g. `"bold"`, or a call to a style function, e.g.
 #'   `epoxy_style_bold()`. `epoxy_style()` chains the style functions together,
