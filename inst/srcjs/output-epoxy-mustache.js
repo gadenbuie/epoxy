49364d8ead6cb0cca64118265cea3b358c51813c
Garrick Aden-Buie <garrick@adenbuie.com>
2023-04-17 13:04:21 -0400

    Use standardjs style

--- inst/srcjs/output-epoxy-mustache.js
+++ inst/srcjs/output-epoxy-mustache.js
@@ -1,26 +1,29 @@
-var epoxyMustacheOutputBinding = new Shiny.OutputBinding();
+/* globals Shiny,$,Mustache */
+
+const epoxyMustacheOutputBinding = new Shiny.OutputBinding()
+
 $.extend(epoxyMustacheOutputBinding, {
-  find: function(scope) {
-    return $(scope).find('.epoxy-mustache');
+  find: function (scope) {
+    return $(scope).find('.epoxy-mustache')
   },
-  renderValue: function(el, data) {
+  renderValue: function (el, data) {
     if (!el.epoxyTemplate) {
       // store template in DOM element and clean up visible markup
-      el.epoxyTemplate = el.dataset.epoxyTemplate;
-      el.removeAttribute('data-epoxy-template');
+      el.epoxyTemplate = el.dataset.epoxyTemplate
+      el.removeAttribute('data-epoxy-template')
     }
-    el.innerHTML = Mustache.render(el.epoxyTemplate, data);
+    el.innerHTML = Mustache.render(el.epoxyTemplate, data)
   },
-  renderError: function(el, err) {
-    this.clearError(el);
+  renderError: function (el, err) {
+    this.clearError(el)
     if (err.message !== '') {
-      console.error('[epoxy] ' + err.message);
-      el.classList.add('epoxy-error');
+      console.error('[epoxy] ' + err.message)
+      el.classList.add('epoxy-error')
     }
-    return;
   },
-  clearError: function(el) {
-    el.classList.remove('epoxy-error');
+  clearError: function (el) {
+    el.classList.remove('epoxy-error')
   }
-});
-Shiny.outputBindings.register(epoxyMustacheOutputBinding, 'shiny.epoxyMustache');
+})
+
+Shiny.outputBindings.register(epoxyMustacheOutputBinding, 'shiny.ui_epoxy_mustache')
--- inst/srcjs/output-epoxy.js
+++ inst/srcjs/output-epoxy.js
@@ -1,63 +1,66 @@
-var epoxyOutputBinding = new Shiny.OutputBinding();
+/* globals Shiny,$ */
+
+const epoxyOutputBinding = new Shiny.OutputBinding()
+
 $.extend(epoxyOutputBinding, {
-  find: function(scope) {
-    return $(scope).find('.epoxy-html');
+  find: function (scope) {
+    return $(scope).find('.epoxy-html')
   },
-  _is_empty: function(x) {
-    if (x === undefined || x === null) return true;
-    if (typeof x === 'number') return false;
-    if (typeof x === 'string') return false;
-    if (typeof x === 'boolean') return false;
-    if (Array.isArray(x) && x.length) return false;
-    if (x instanceof Object && Object.keys(x).length) return false;
-    return true;
+  _is_empty: function (x) {
+    if (x === undefined || x === null) return true
+    if (typeof x === 'number') return false
+    if (typeof x === 'string') return false
+    if (typeof x === 'boolean') return false
+    if (Array.isArray(x) && x.length) return false
+    if (x instanceof Object && Object.keys(x).length) return false
+    return true
   },
-  renderValue: function(el, data) {
+  renderValue: function (el, data) {
     // remove copies of epoxyItem
-    let elCopies = el.querySelectorAll('[data-epoxy-copy]')
+    const elCopies = el.querySelectorAll('[data-epoxy-copy]')
     elCopies.forEach(e => e.parentElement.removeChild(e))
 
-    let items = el.querySelectorAll('[data-epoxy-item]');
+    const items = el.querySelectorAll('[data-epoxy-item]')
     items.forEach(item => {
-      item.classList.remove('epoxy-item__placeholder');
-      let itemName = item.dataset.epoxyItem;
+      item.classList.remove('epoxy-item__placeholder')
+      const itemName = item.dataset.epoxyItem
 
-      let itemData = data[itemName];
+      let itemData = data[itemName]
       if (this._is_empty(itemData)) {
-        item.style.display = 'none';
-        return;
+        item.style.display = 'none'
+        return
       } else {
         item.style.removeProperty('display')
       }
       if (itemData instanceof Array) {
-        let lastItem = item;
-        item.innerHTML = itemData[0];
-        itemParent = item.parentElement;
-        itemData = itemData.slice(1);
-        for (let itemDataThis of itemData) {
-          const itemNew = item.cloneNode();
-          itemNew.removeAttribute('data-epoxy-item');
-          itemNew.dataset.epoxyCopy = itemName;
-          itemNew.innerHTML = itemDataThis;
-          itemParent.insertBefore(itemNew, lastItem.nextSibling);
-          lastItem = itemNew;
+        let lastItem = item
+        item.innerHTML = itemData[0]
+        const itemParent = item.parentElement
+        itemData = itemData.slice(1)
+        for (const itemDataThis of itemData) {
+          const itemNew = item.cloneNode()
+          itemNew.removeAttribute('data-epoxy-item')
+          itemNew.dataset.epoxyCopy = itemName
+          itemNew.innerHTML = itemDataThis
+          itemParent.insertBefore(itemNew, lastItem.nextSibling)
+          lastItem = itemNew
         }
       } else {
-        item.innerHTML = itemData;
+        item.innerHTML = itemData
       }
-    });
-    el.classList.remove('epoxy-init');
+    })
+    el.classList.remove('epoxy-init')
   },
-  renderError: function(el, err) {
-    this.clearError(el);
+  renderError: function (el, err) {
+    this.clearError(el)
     if (err.message !== '') {
-      console.error('[epoxy] ' + err.message);
-      el.classList.add('epoxy-error');
+      console.error('[epoxy] ' + err.message)
+      el.classList.add('epoxy-error')
     }
-    return;
   },
-  clearError: function(el) {
-    el.classList.remove('epoxy-error');
+  clearError: function (el) {
+    el.classList.remove('epoxy-error')
   }
-});
-Shiny.outputBindings.register(epoxyOutputBinding, 'shiny.ui_epoxy_html');
+})
+
+Shiny.outputBindings.register(epoxyOutputBinding, 'shiny.ui_epoxy_html')
