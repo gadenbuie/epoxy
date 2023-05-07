49a2ec8a097b2f060a2a594068dd8d90b9add7ac
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-07 08:21:57 -0400

    Add tests for deprecated functions

---
+++ tests/testthat/test-deprecated.R
@@ -0,0 +1,85 @@
+# test_that()
+
+describe("epoxy_style() functions are deprecated", {
+	env <- rlang::env(word = "abc")
+
+	it("deprecated epoxy_style()", {
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style("bold")("word", env),
+				"**abc**"
+			)
+		)
+	})
+
+	it("epoxy_style_bold", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_bold()("word", env),
+        "**abc**"
+      )
+    )
+  })
+
+	it("epoxy_style_code", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_code()("word", env),
+        "`abc`"
+      )
+    )
+  })
+	it("epoxy_style_collapse", {
+    env <- rlang::env(word = c("a", "b", "c"))
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_collapse()("word*", env),
+        "a, b, c"
+      )
+    )
+  })
+
+	it("epoxy_style_html", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_html()("strong word", env),
+        html_chr("<strong>abc</strong>")
+      )
+    )
+  })
+
+	it("epoxy_style_inline", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_inline()(".code word", env),
+        "`abc`"
+      )
+    )
+  })
+
+	it("epoxy_style_italic", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_italic()("word", env),
+        "_abc_"
+      )
+    )
+  })
+
+	it("epoxy_style_get", {
+    lifecycle::expect_deprecated(epoxy_style_get())
+  })
+
+	it("epoxy_style_set", {
+    lifecycle::expect_deprecated(epoxy_style_set())
+  })
+
+  it("epoxy_style_wrap", {
+    lifecycle::expect_deprecated(
+      expect_equal(
+        epoxy_style_wrap(before = "-")("word", env),
+        "-abc-"
+      )
+    )
+  })
+})
