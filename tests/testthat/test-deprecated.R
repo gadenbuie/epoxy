871f0879cb0c99a98c7bbab07e9a7a54e3c56a97
gadenbuie <gadenbuie@users.noreply.github.com>
2023-05-07 12:27:23 +0000

    Fix indents and invisible characters (GitHub Actions)

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -12,7 +12,7 @@ describe("epoxy_style() functions are deprecated", {
 		)
 	})
 
-  it("deprecated epoxy_style_apply()", {
+	it("deprecated epoxy_style_apply()", {
 		lifecycle::expect_deprecated(
 			expect_equal(
 				epoxy_style_apply(toupper)("word", env),
@@ -22,73 +22,73 @@ describe("epoxy_style() functions are deprecated", {
 	})
 
 	it("epoxy_style_bold", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_bold()("word", env),
-        "**abc**"
-      )
-    )
-  })
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_bold()("word", env),
+				"**abc**"
+			)
+		)
+	})
 
 	it("epoxy_style_code", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_code()("word", env),
-        "`abc`"
-      )
-    )
-  })
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_code()("word", env),
+				"`abc`"
+			)
+		)
+	})
 	it("epoxy_style_collapse", {
-    env <- rlang::env(word = c("a", "b", "c"))
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_collapse()("word*", env),
-        "a, b, c"
-      )
-    )
-  })
+		env <- rlang::env(word = c("a", "b", "c"))
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_collapse()("word*", env),
+				"a, b, c"
+			)
+		)
+	})
 
 	it("epoxy_style_html", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_html()("strong word", env),
-        html_chr("<strong>abc</strong>")
-      )
-    )
-  })
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_html()("strong word", env),
+				html_chr("<strong>abc</strong>")
+			)
+		)
+	})
 
 	it("epoxy_style_inline", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_inline()(".code word", env),
-        "`abc`"
-      )
-    )
-  })
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_inline()(".code word", env),
+				"`abc`"
+			)
+		)
+	})
 
 	it("epoxy_style_italic", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_italic()("word", env),
-        "_abc_"
-      )
-    )
-  })
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_italic()("word", env),
+				"_abc_"
+			)
+		)
+	})
 
 	it("epoxy_style_get", {
-    lifecycle::expect_deprecated(epoxy_style_get())
-  })
+		lifecycle::expect_deprecated(epoxy_style_get())
+	})
 
 	it("epoxy_style_set", {
-    lifecycle::expect_deprecated(epoxy_style_set())
-  })
+		lifecycle::expect_deprecated(epoxy_style_set())
+	})
 
-  it("epoxy_style_wrap", {
-    lifecycle::expect_deprecated(
-      expect_equal(
-        epoxy_style_wrap(before = "-")("word", env),
-        "-abc-"
-      )
-    )
-  })
+	it("epoxy_style_wrap", {
+		lifecycle::expect_deprecated(
+			expect_equal(
+				epoxy_style_wrap(before = "-")("word", env),
+				"-abc-"
+			)
+		)
+	})
 })
