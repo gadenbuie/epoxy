841ecd47e01ad2ed8ae8c9c58a7ade32c87606d0
Garrick Aden-Buie <garrick@adenbuie.com>
2023-05-17 09:12:12 -0400

    Add `ui_epoxy_markdown()` example app

--- R/shiny.R
+++ R/shiny.R
@@ -224,6 +224,77 @@ ui_epoxy_html <- function(
 #'   [commonmark::markdown_html()].
 #' @inheritParams ui_epoxy_html
 #'
+#' @examplesIf rlang::is_installed("shiny")
+#' library(shiny)
+#'
+#' movie_ids <- rlang::set_names(
+#'   bechdel$imdb_id,
+#'   bechdel$title
+#' )
+#'
+#' ui <- fixedPage(
+#'   fluidRow(
+#'     column(
+#'       width = 3,
+#'       selectInput("movie", "Movie", movie_ids),
+#'       uiOutput("poster")
+#'     ),
+#'     column(
+#'       width = 9,
+#'       ui_epoxy_markdown(
+#'         .id = "about_movie",
+#'         # .markdown_fn = commonmark::markdown_html,
+#'         "
+#' ## {{title}}
+#'
+#' **Released:** {{ year }} \\
+#' **Rated:** {{ rated }} \\
+#' **IMDB Rating:** {{ imdb_rating }}
+#'
+#' _{{ title }}_ is {{ genre }} film released in {{ year }}.
+#' It was filmed in {{ country }} with a budget of {{ budget }}
+#' and made {{ domgross }} at the box office.
+#' _{{ title }}_ recieved a Bechdel rating of **{{ bechdel_rating }}**
+#' for the following plot:
+#'
+#' > {{ plot }}
+#' "
+#'       )
+#'     )
+#'   )
+#' )
+#'
+#' server <- function(input, output, session) {
+#'   movie <- reactive({
+#'     movie <- bechdel[bechdel$imdb_id == input$movie, ]
+#'     movie$budget <- epoxy("{.dollar budget}", .data = movie)
+#'     movie$domgross <- epoxy("{.dollar domgross}", .data = movie)
+#'
+#'     vowels <- c("a", "e", "i", "o", "u")
+#'     movie$genre  <- paste(
+#'       ifelse(substr(tolower(movie$genre), 1, 1) %in% vowels, "an", "a"),
+#'       tolower(movie$genre)
+#'     )
+#'
+#'     movie
+#'   })
+#'
+#'   output$about_movie <- render_epoxy(.list = movie())
+#'   output$poster <- renderUI(
+#'     img(
+#'       src = movie()$poster,
+#'       alt = paste0("Poster for ", movie()$title),
+#'       style = "max-height: 400px; max-width: 100%; margin: 0 auto; display: block;"
+#'     )
+#'   )
+#' }
+#'
+#' if (interactive()) {
+#'   shinyApp(ui, server)
+#' }
+#'
+#' @eval write_epoxy_example_app("ui_epoxy_markdown")
+#'
 #' @seealso [ui_epoxy_html()], [ui_epoxy_mustache()], [render_epoxy()]
 #' @return An HTML object.
 #' @export
@@ -668,16 +739,16 @@ write_epoxy_example_app <- function(name, fn_name = paste0(name, "()")) {
 #' run_epoxy_example_app(name = NULL)
 #'
 #' @param name Name of the example, currently one of `"ui_epoxy_html"`,
-#'   `"ui_epoxy_mustache"`, or `"render_epoxy"`.
+#'   `"ui_epoxy_markdown"`, `"ui_epoxy_mustache"`, or `"render_epoxy"`.
 #' @inheritParams shiny::runApp
 #' @inheritDotParams shiny::runApp -display.mode
 #'
 #' @return Runs the Shiny example app interactively. Nothing is returned.
 #'
-#' @seealso [ui_epoxy_html()], [ui_epoxy_mustache()], [render_epoxy()]
+#' @seealso [ui_epoxy_html()], [ui_epoxy_markdown()], [ui_epoxy_mustache()], [render_epoxy()]
 #' @export
 run_epoxy_example_app <- function(
-	name = c("ui_epoxy_html", "ui_epoxy_mustache", "render_epoxy"),
+	name = c("ui_epoxy_html", "ui_epoxy_markdown", "ui_epoxy_mustache", "render_epoxy"),
 	display.mode = "showcase",
 	...
 ) {
---
+++ inst/examples/ui_epoxy_markdown/app.R
@@ -0,0 +1,67 @@
+# Generated from example in ui_epoxy_markdown(): do not edit by hand
+library(shiny)
+library(epoxy)
+
+movie_ids <- rlang::set_names(
+  bechdel$imdb_id,
+  bechdel$title
+)
+
+ui <- fixedPage(
+  fluidRow(
+    column(
+      width = 3,
+      selectInput("movie", "Movie", movie_ids),
+      uiOutput("poster")
+    ),
+    column(
+      width = 9,
+      ui_epoxy_markdown(
+        .id = "about_movie",
+        # .markdown_fn = commonmark::markdown_html,
+        "
+## {{title}}
+
+**Released:** {{ year }} \\
+**Rated:** {{ rated }} \\
+**IMDB Rating:** {{ imdb_rating }}
+
+_{{ title }}_ is {{ genre }} film released in {{ year }}.
+It was filmed in {{ country }} with a budget of {{ budget }}
+and made {{ domgross }} at the box office.
+_{{ title }}_ recieved a Bechdel rating of **{{ bechdel_rating }}**
+for the following plot:
+
+> {{ plot }}
+"
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  movie <- reactive({
+    movie <- bechdel[bechdel$imdb_id == input$movie, ]
+    movie$budget <- epoxy("{.dollar budget}", .data = movie)
+    movie$domgross <- epoxy("{.dollar domgross}", .data = movie)
+
+    vowels <- c("a", "e", "i", "o", "u")
+    movie$genre  <- paste(
+      ifelse(substr(tolower(movie$genre), 1, 1) %in% vowels, "an", "a"),
+      tolower(movie$genre)
+    )
+
+    movie
+  })
+
+  output$about_movie <- render_epoxy(.list = movie())
+  output$poster <- renderUI(
+    img(
+      src = movie()$poster,
+      alt = paste0("Poster for ", movie()$title),
+      style = "max-height: 400px; max-width: 100%; margin: 0 auto; display: block;"
+    )
+  )
+}
+
+shinyApp(ui, server)
