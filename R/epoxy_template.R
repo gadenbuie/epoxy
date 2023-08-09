cb22c65f325ac26948afde8a80bb1434c1800bec
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-09 07:10:58 -0400

    feat(epoxy_template_chunk): Reuse chunks

--- R/epoxy_template.R
+++ R/epoxy_template.R
@@ -1,4 +1,64 @@
-epoxy_template_chunk <- function(.data, label, ...) {
+#' Reuse a Template Chunk
+#'
+#' @description
+#' Reuse a chunk
+#'
+#' ## Use in R Markdown or Quarto
+#'
+#' ``````
+#' ```{epoxy movie-release}
+#' {.emph title} was released in {year}.
+#' ```
+#'
+#' ```{r}
+#' # Re-using the template we defined above
+#' epoxy_template_chunk(bechdel[1, ], "movie-release")
+#' ```
+#'
+#' ```{r}
+#' # Using in a dplyr pipeline
+#' bechdel |>
+#'   dplyr::filter(year == 1989) |>
+#'   epoxy_template_chunk("movie-release")
+#' ```
+#' ``````
+#'
+#' Or you can even use it inline:
+#'
+#' ``````
+#' > `r epoxy_template_chunk(bechdel[2, ], "movie-release")`
+#' ``````
+#'
+#' ## Template Options
+#'
+#' When rendering a template, `epoxy_template_chunk()` will inherit the options
+#' set in a number of different ways. The final template options are determined
+#' in the following order, ranked by importance. Options set in a higher-ranked
+#' location will override options set in a lower-ranked location.
+#'
+#' 1. The arguments passed to `epoxy_template_chunk()`, such as `.data` or any
+#'    arguments passed in the `...`. These options always have preference over
+#'    options set anywhere else.
+#'
+#' 2. The chunk options from the template chunk. These options typically are
+#'    relevant to the template itself, such as the engine used or the opening
+#'    and closing delimiters.
+#'
+#' 3. The current knitr options for the chunk where the template is rendered.
+#'    In most cases, this will be the global knitr options, but you could still
+#'    override these options locally in the chunk where you render the template.
+#'
+#' @param label The chunk label, i.e. the human-readable name of the chunk.
+#' @inheritDotParams epoxy
+#' @inheritParams epoxy
+#'
+#' @return A character string of the rendered template based on the `label`
+#'   chunk. The results are marked as `"asis"` output so that they are treated
+#'   as regular text rather than being displayed as code results.
+#'
+#' @family Templating functions
+#' @export
+epoxy_template_chunk <- function(.data = NULL, label, ...) {
 	if (!rlang::is_string(label)) {
 		rlang::abort("`label` must be a string")
 	}
@@ -44,7 +104,7 @@ epoxy_template_chunk <- function(.data, label, ...) {
 		options = opts
 	)
 
-	eval(call)
+	knitr::asis_output(eval(call))
 }
 
 knitr_chunk_get <- function(label) {
